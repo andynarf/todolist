5bcd347b567a9fbf038515c3e6c6a0572b555e90
sofia barrantes gamboa <sofiabarrantes.gamboa@gmail.com>
2020-02-27 17:43:25 -0300

    added a function file for the js, start calling the elements

---
+++ src/functions.js
@@ -0,0 +1,11 @@
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
+
+
