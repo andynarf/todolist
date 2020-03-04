b07ba9dd9c1cedf99a0273f4b6b055c3499dad95
andynarf <untalandy@gmail.com>
2020-03-04 13:50:13 -0300

    fixed problems with date and priority not showing after rfreshing page.

--- src/index.js
+++ src/index.js
@@ -1,33 +1,34 @@
-import "./css/style.css";
-import skeleton from "./js/skeleton";
+import './css/style.css';
+import skeleton from './js/skeleton';
 
-document.getElementById("skeletonid").innerHTML= skeleton;
+document.getElementById('skeletonid').innerHTML = skeleton;
 
-const dateElement = document.getElementById("date");
-const list = document.getElementById("list");
-const input = document.getElementById("input");
-const date = document.getElementById("start");
-const priority = document.getElementById("priority");
-const description = document.getElementById("description");
-const submit = document.getElementById("submit");
+const dateElement = document.getElementById('date');
+const list = document.getElementById('list');
+const input = document.getElementById('input');
+const date = document.getElementById('start');
+const priority = document.getElementById('priority');
+const description = document.getElementById('description');
+const submit = document.getElementById('submit');
 
-const CHECK = "fa-check-circle";
-const UNCHECK = "fa-circle-thin";
-const LINE_THROUGH = "lineThrough";
+const CHECK = 'fa-check-circle';
+const UNCHECK = 'fa-circle-thin';
+const LINE_THROUGH = 'lineThrough';
 
-let myStorage = JSON.parse(window.localStorage.getItem("TODOS"));
+let myStorage = JSON.parse(window.localStorage.getItem('TODOS'));
 if (myStorage == null) {
   myStorage = [];
 }
 
-let id = window.localStorage.getItem("ids");
+let id = window.localStorage.getItem('ids');
 if (id == null) {
   id = 0;
 }
 
 function updateLocalStorage(array) {
-  window.localStorage.setItem("TODOS", JSON.stringify(array));
-  window.localStorage.setItem("ids", id); }
+  window.localStorage.setItem('TODOS', JSON.stringify(array));
+  window.localStorage.setItem('ids', id);
+}
 
 const loadToDo = localStorage => {
   localStorage.forEach(elem => {
@@ -38,25 +39,24 @@ const loadToDo = localStorage => {
       elem.trash,
       elem.priority,
       elem.description,
-      elem.date
+      elem.date,
     );
   });
 };
 
 
-
-const options = { weekday: "long", month: "short", day: "numeric" };
+const options = { weekday: 'long', month: 'short', day: 'numeric' };
 const today = new Date();
-dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+dateElement.innerHTML = today.toLocaleDateString('en-US', options);
 
-const addToDo = (toDo, id, done, trash, priority, description) => {
+const addToDo = (toDo, id, done, trash, priority, description, date) => {
   if (trash) {
     return;
   }
 
   const DONE = done ? CHECK : UNCHECK;
-  const LINE = done ? LINE_THROUGH : "";
-  const position = "beforeend";
+  const LINE = done ? LINE_THROUGH : '';
+  const position = 'beforeend';
   const item = `
     <li class="item">
     <i class="fa ${DONE} co" job="complete" id="${id}"></i>
@@ -64,8 +64,8 @@ const addToDo = (toDo, id, done, trash, priority, description) => {
     <div class="dropdown">
     <button class="dropbtn">Details</button>
     <div class="dropdown-content">
-      <a href="#"><b>Date:</b> ${date.value}</a>
-      <a href="#"><b>Priority:</b> ${priority.value}</a>
+      <a href="#"><b>Date:</b> ${date}</a>
+      <a href="#"><b>Priority:</b> ${priority}</a>
       <a href="#"><b>Description:</b> ${description}</a>
     </div>
   </div> 
@@ -74,35 +74,36 @@ const addToDo = (toDo, id, done, trash, priority, description) => {
   list.insertAdjacentHTML(position, item);
 };
 
-submit.addEventListener("click", event => {
+submit.addEventListener('click', () => {
   const toDo = input.value;
   const textarea = description.value;
   const datevalue = date.value;
+  const prior = priority.value;
   if (toDo) {
-    addToDo(toDo, id, false, false, priority, textarea, datevalue);
+    addToDo(toDo, id, false, false, prior, textarea, datevalue);
     myStorage.push({
       name: toDo,
-      id: id,
+      id,
       done: false,
       trash: false,
-      priority: priority.value,
+      priority: prior,
       description: textarea,
-      date: datevalue
+      date: datevalue,
     });
     id++;
     updateLocalStorage(myStorage);
     console.log(myStorage);
   }
-  input.value = "";
-  description.value = "";
+  input.value = '';
+  description.value = '';
 });
 
 const completeToDo = element => {
   element.classList.toggle(CHECK);
   element.classList.toggle(UNCHECK);
-  element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+  element.parentNode.querySelector('.text').classList.toggle(LINE_THROUGH);
 
-  myStorage[element.id].done = myStorage[element.id].done ? false : true;
+  myStorage[element.id].done = !myStorage[element.id].done;
 };
 
 const removeToDo = element => {
@@ -112,19 +113,19 @@ const removeToDo = element => {
   console.log(myStorage);
 };
 
-list.addEventListener("click", event => {
+list.addEventListener('click', event => {
   const element = event.target;
   const elementJob = element.attributes.job.value;
 
-  if (elementJob == "complete") {
+  if (elementJob == 'complete') {
     completeToDo(element);
     updateLocalStorage(myStorage);
-  } else if (elementJob == "delete") {
+  } else if (elementJob == 'delete') {
     removeToDo(element);
     updateLocalStorage(myStorage);
   }
 });
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   loadToDo(myStorage);
 });
--- src/js/createitems.js
+++ src/js/createitems.js
@@ -1,25 +1,38 @@
-const addToDo = (toDo, id, done, trash, priority, description) => {
-    if (trash) {
-      return;
-    }
-    
-    const DONE = done ? CHECK : UNCHECK;
-    const LINE = done ? LINE_THROUGH : "";
-    const position = "beforeend";
-    const item = `
+const CHECK = 'fa-check-circle';
+const UNCHECK = 'fa-circle-thin';
+const LINE_THROUGH = 'lineThrough';
+const list = document.getElementById('list');
+
+
+const addToDo = (toDo, id, done, trash, priority, description, date) => {
+  if (trash) {
+    return;
+  }
+
+  const DONE = done ? CHECK : UNCHECK;
+  const LINE = done ? LINE_THROUGH : '';
+  const position = 'beforeend';
+  const item = `
       <li class="item">
       <i class="fa ${DONE} co" job="complete" id="${id}"></i>
       <p class="text ${LINE} co"> ${toDo}  </p>
       <div class="dropdown">
-      <button class="dropbtn">Dropdown</button>
+      <button class="dropbtn">Details</button>
       <div class="dropdown-content">
-        <a href="#">priority: ${priority}</a>
-        <a href="#">description: ${description}</a>
+        <a href="#"><b>Date:</b> ${date}</a>
+        <a href="#"><b>Priority:</b> ${priority}</a>
+        <a href="#"><b>Description:</b> ${description}</a>
       </div>
     </div> 
       <i class="fa fa-trash-o de" job="delete" id="${id}" ></i>
       </li>`;
-    list.insertAdjacentHTML(position, item);
-  };
+  list.insertAdjacentHTML(position, item);
+};
 
-  export {addToDo}
+
+export {
+  addToDo,
+  CHECK,
+  UNCHECK,
+  LINE_THROUGH,
+};
